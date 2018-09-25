66144cd3962b62689fe6eb8eb91d23327374eb68
Siddharth Krishnamurthy <sidkrishna92@gmail.com>
2018-09-25 11:55:54 -0500

    UDF for extracting probability value from Vector element

--- driver.py
+++ driver.py
@@ -17,7 +17,10 @@ Output:
 
 def driver():
 	from pyspark import SparkContext, SparkConf
-	from pyspark.sql import SQLContext, HiveContext, SparkSession				
+	from pyspark.sql import SQLContext, HiveContext, SparkSession
+	from pyspark.sql.functions import monotonically_increasing_id, lit
+	from pyspark.sql.types import FloatType
+
 	spark = SparkSession.builder.master("yarn").appName('Comment_Classification').enableHiveSupport().getOrCreate()
 	spark.sparkContext.setLogLevel('ERROR')
 	
@@ -50,6 +53,16 @@ def driver():
 	print("\n"+ "--"*5)
 	print("Test Set number of rows: ", test_data.data_df.count())
 	
+	def ith_(v, i):
+                try:
+                        return float(v[i])
+                except ValueError:
+                        return None
+
+	ith = spark.udf.register("ith", ith_, FloatType())
+	final_df = test_data.data_df.select('comment_text')
+	final_df = final_df.withColumn('row_index', monotonically_increasing_id())
+	
 	for i,label in enumerate(target):
 		#Build and evaluate model on training/validation set
 		model_build = cm.classification_model(train_data.data_df, spark, feat_vec, label, model_name)
@@ -57,9 +70,17 @@ def driver():
 
 		#Run model on Test Data
 		test_model = tm.model_tester(test_data.data_df, spark, feat_vec, label, model_name, model_build.model) 
-		final_df = test_model.pred_df.withColumn(label, test_model.pred_df["probability"])
-	
-	#final_df.coalesce(1).write.csv("./spitzer/results/")
+		test_model.pred_df = test_model.pred_df.withColumn('row_index', monotonically_increasing_id())
+
+		#test_model.pred_df.show()
+		test_model.pred_df = test_model.pred_df.withColumn("p1", ith("probability", lit(0)))
+		test_model.pred_df = test_model.pred_df.drop("probability")
+
+		final_df = final_df.join(test_model.pred_df, on=["row_index"]).sort("row_index").withColumnRenamed("p1", label)
+
+	final_df = final_df.drop("row_index", "comment_text")
+	final_df.show()
+	final_df.coalesce(1).write.csv("./spitzer/results/")
 	
 if __name__ == "__main__":
 	driver()
